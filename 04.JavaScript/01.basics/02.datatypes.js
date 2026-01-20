e7182f1e17d332485a7f08281e675147b811d21e
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-01-20 20:19:53 +0530

    add datatypes file

---
+++ 04.JavaScript/01.basics/02.datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not the browser
+
+console.log(3 + 3);
+// code readabi;ity should be high
+console.log("TAUFEEQ");
+
+let name = "TAUFEEQ";
+let age = 17;
+let isLoggedIn = false;
+
+// number=> 2 to the power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object, null is object
