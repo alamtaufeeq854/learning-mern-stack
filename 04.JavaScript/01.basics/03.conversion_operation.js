d1a436277a0ba8c3736328a4d3533b479f369bcd
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-01-26 10:19:33 +0530

    add conversion operation file

---
+++ 04.JavaScript/01.basics/03.conversion_operation.js
@@ -0,0 +1,60 @@
+let marks = "Taufeeq";
+
+console.log(typeof marks);
+
+let valueInNumber = Number(marks);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "55" => 55
+//"55abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Taufeeq";
+let boolean_Of_isLoggedIn = Boolean(isLoggedIn);
+console.log(boolean_Of_isLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "taufeeq" => true
+
+let someNumber = 56;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *************** Operations **********************
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "TAUFEEQ";
+let str2 = " ALAM";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(2 + "3");
+console.log("2" + 3);
+console.log("2" + 3 + 1);
+console.log(2 + 3 + "1");
+
+console.log(+true);
+console.log(+"");
+
+let marks1, marks2, marks3, marks4;
+
+marks1 = marks2 = marks3 = marks4 = 56 + 43;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
