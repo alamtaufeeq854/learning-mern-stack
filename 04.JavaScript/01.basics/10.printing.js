7726ceeb4ee9ce6cdbdbc5515f63cfba2349f1a3
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-04 06:53:24 +0530

    add printing file

---
+++ 04.JavaScript/01.basics/10.printing.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+process.stdout.write("world");
+process.stdout.write(" how are you");
+console.log("Hello");
+
+/* 
+
+Each console.log("") have new line but
+process.stdout.write("") doesn't have any new line 
+
+*/
+
+console.table({ country: "India" });
+console.warn({ country: "India" });
