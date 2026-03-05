13474427e5fe8fda11d73724892769077678bd5b
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-05 10:23:55 +0530

    add datatype file

---
+++ 04.JavaScript/01.basics/11.datatype.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+// console.log("World !");
+
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "Taufeeq";
+let isLoggedIn = false;
+
+// objects
+
+let drinksTypes = ["Milk", "Tea", "Water", "Softdrink"];
+let user = { firstname: "Taufeeq", lastname: "Alam" };
+
+let getScore = score;
+
+console.log(getScore);
