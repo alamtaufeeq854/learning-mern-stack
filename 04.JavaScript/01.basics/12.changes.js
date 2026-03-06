88c452026565927761ea1119b922f407f378ba4a
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-06 07:15:19 +0530

    add changes file

---
+++ 04.JavaScript/01.basics/12.changes.js
@@ -0,0 +1,16 @@
+let gameName = "spiderman";
+
+gameName = "Batman";
+
+console.log(gameName);
+
+const username = "Taufeeq";
+
+/*
+
+username = "Alam";
+
+const variable can't be change.
+
+*/
+console.log(username);
