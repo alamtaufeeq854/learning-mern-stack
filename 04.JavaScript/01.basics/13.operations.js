10ea5ffec8b1b19f4eab19af5ff11971a352b06d
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-07 07:09:15 +0530

    add operations file

---
+++ 04.JavaScript/01.basics/13.operations.js
@@ -0,0 +1,28 @@
+let score = 4;
+let bonus = 5;
+
+let totalScore = score + bonus;
+
+let addition = 8 + 6;
+let subtract = 8 - 5;
+let multiply = 2 * 6;
+let divide = 8 / 2;
+let remainder = 7 % 2;
+let expo = 2 ** 6;
+
+let myscore = 110;
+myscore++;
+
+let credits = 59;
+credits--;
+
+// Comparision Operations
+
+let num1 = 8;
+let num2 = 8;
+let num3 = 9;
+
+console.log(num1 == num2); // true
+console.log(num1 != num3); // true
+console.log(num1 > num3); // false
+console.log(num1 < num3); // true
