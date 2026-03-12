8e5c8cfcc2da37866cb4769543b6eaebea4ba19a
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-12 22:24:41 +0530

    add non primitives file

---
+++ 04.JavaScript/02.basics/05.non_primitives.js
@@ -0,0 +1,27 @@
+const username = {
+  "first name": "Taufeeq",
+  isLoggedin: true,
+};
+
+username.firstname = "Mr. T";
+username.lastname = "Alam";
+
+// console.log(username["first name"]);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+
+let anotherUser = ["Taufeeq", true];
+
+// console.log(anotherUser[0]);
+
+let isValue = "5xyz";
+console.log(typeof Number(isValue));
+console.log(Number(isValue));
+console.log(Number(null));
+console.log(Number(undefined));
