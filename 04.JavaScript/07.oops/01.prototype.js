20126cecf901797d2dfae255f00fc33466ac24dc
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-18 17:54:42 +0530

    add prototype file

---
+++ 04.JavaScript/07.oops/01.prototype.js
@@ -0,0 +1,13 @@
+let computer = { cpu: 12 };
+let lenovo = { screen: "HD", __proto__: computer };
+let tom_Hardware = {};
+
+// console.log(`Computer`, lenovo.__proto__);
+
+let genericCar = { tyres: 4 };
+
+let tesla = { driver: "AI" };
+
+Object.setPrototypeOf(tesla, genericCar);
+
+console.log(`Tesla`, Object.getPrototypeOf(tesla));
