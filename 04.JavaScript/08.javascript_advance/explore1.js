d0df14f158172094f9ceec40ab012ba2578bb7fd
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-28 07:38:58 +0530

    add explore file

---
+++ 04.JavaScript/08.javascript_advance/explore1.js
@@ -0,0 +1,13 @@
+console.log("Taufeeq");
+
+function Hello() {
+  console.log("Hello my dear friend !");
+}
+
+setTimeout(() => {
+  Hello();
+}, 3000);
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
