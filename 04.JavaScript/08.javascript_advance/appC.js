0ad55e8407f79bbc18f256204e96e28e8feba42a
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-03-31 20:28:45 +0530

    add app files

---
+++ 04.JavaScript/08.javascript_advance/appC.js
@@ -0,0 +1,2 @@
+const mathOperations = require("./math_operationsC.js");
+console.log(mathOperations.add(4, 13));
---
+++ 04.JavaScript/08.javascript_advance/appM.js
@@ -0,0 +1,10 @@
+// Module import file
+
+// default import
+import multiply from "./math_operationsM.js";
+
+// named import
+import { add, subtract } from "./math_operationsM.js";
+
+console.log(multiply(4, 10));
+console.log(add(5, 7));
