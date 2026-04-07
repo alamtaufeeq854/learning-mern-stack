cc413128308c936e3b53bd379cbd4ed630a1de70
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-04-07 07:55:16 +0530

    add expense tracker files

---
+++ 04.JavaScript/09.js_projects/expense_tracker/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+  updateTotal();
+  renderExpenses();
+
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name: name,
+        amount: amount,
+      };
+
+      expenses.push(newExpense);
+      saveExpensesToLocal();
+      renderExpenses();
+      updateTotal();
+
+      expenseAmountInput.value = "";
+      expenseNameInput.value = "";
+    }
+  });
+  function calculateTotal() {
+    return expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+
+  function saveExpensesToLocal() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+
+  function updateTotal() {
+    total = calculateTotal();
+    totalAmountDisplay.textContent = total.toFixed(2);
+  }
+
+  function renderExpenses() {
+    expenseList.innerHTML = "";
+    expenses.forEach((expense) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${expense.name}: $${expense.amount}
+       <button data-id="${expense.id}">Delete</button>`;
+
+      expenseList.appendChild(li);
+    });
+  }
+
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+
+      saveExpensesToLocal();
+      renderExpenses();
+      updateTotal();
+    }
+  });
+});
