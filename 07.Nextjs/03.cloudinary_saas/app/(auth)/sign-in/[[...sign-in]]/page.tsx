e868470cf310a4859e6e64f4951ceeea1fc154e2
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-09-26 16:52:02 +0530

    improving ui

--- 07.Nextjs/03.cloudinary_saas/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ 07.Nextjs/03.cloudinary_saas/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center mt-2">
+    <div className="min-h-screen flex justify-center items-center mt-2">
       <SignIn />
     </div>
   );
